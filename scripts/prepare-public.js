fc2c4452073ea697d8f6485d25fc356aea45d11c
Shirzad01 <shirzadharoon07@gmail.com>
2026-04-18 05:31:19 +0430

    resolve

--- scripts/prepare-public.js
+++ scripts/prepare-public.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const files = [
     'index.html', 'style.css', 'wishlist.html', 'cart.html', 'checkout.html',
